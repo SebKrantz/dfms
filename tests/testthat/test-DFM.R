a68ead4b7b42f55f001b71743234aab80a91c290
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2022-09-16 01:35:34 +0200

    Testing methods and helpers.

--- tests/testthat/test-DFM.R
+++ tests/testthat/test-DFM.R
@@ -7,13 +7,41 @@ BM14[, BM14_Models$log_trans] %<>% log()
 BM14[, BM14_Models$freq == "M"] %<>% diff()
 BM14[, BM14_Models$freq == "Q"] %<>% diff(3)
 
+### Missing value removal
+for(narm in c("LE", "all")) {
+  for(m in c("median", "rnorm", "median.ma", "median.ma.spline"))
+    expect_false(anyNA(tsremimpNA(BM14, na.rm.method = narm, na.impute = m)))
+}
+
+
 ### Small Model ---------------------------------------
 
+ics = ICr(BM14[, BM14_Models$small], max.r = 5)
+expect_output(print(ics))
+
 # Estimating the model with 2 factors and 3 lags
-dfm_small = DFM(BM14[, BM14_Models$small], 2, 3, em.method = "BM")
+dfm_small = DFM(BM14[, BM14_Models$small], 2, 3, em.method = "BM", check.increased = TRUE)
 
 expect_equal(dfm_small$A[1, 1], 1.21068973, tolerance = 1e-5)
 expect_equal(dfm_small$A[2, 6], 0.391438990, tolerance = 1e-5)
+
+# Rudimentry testing various methods
+expect_output(print(dfm_small))
+expect_visible(summary(dfm_small))
+expect_true(is.matrix(fitted(dfm_small)))
+expect_true(is.matrix(residuals(dfm_small)))
+expect_true(ncol(as.data.frame(dfm_small)) == 4L)
+expect_true(ncol(as.data.frame(dfm_small, pivot = "wide.method")) == 5L)
+expect_true(ncol(as.data.frame(dfm_small, pivot = "wide.factor")) == 4L)
+expect_true(ncol(as.data.frame(dfm_small, pivot = "wide")) == 7L)
+expect_true(ncol(as.data.frame(dfm_small, method = "qml", pivot = "wide")) == 3L)
+expect_true(ncol(as.data.frame(dfm_small, pivot = "t.wide")) == 7L)
+expect_true(is.list(predict(dfm_small)))
+expect_output(print(predict(dfm_small)))
+expect_true(ncol(as.data.frame(predict(dfm_small))) == 4L)
+expect_true(ncol(as.data.frame(predict(dfm_small), pivot = "wide")) == 4L)
+expect_true(ncol(as.data.frame(predict(dfm_small), use = "data", pivot = "wide")) == 16L)
+expect_true(ncol(as.data.frame(predict(dfm_small), use = "both", pivot = "wide")) == 18L)
 
 ### Medium-Sized Model ---------------------------------
 
@@ -31,6 +59,10 @@ dfm_large = DFM(BM14, 6, 3, em.method = "BM")
 expect_equal(dfm_large$A[1, 1], 0.48915420, tolerance = 1e-5)
 expect_equal(dfm_large$A[6, 18], 0.10027110, tolerance = 1e-5)
 
+# Twostep estimates
+dfm_large_2s = DFM(BM14, 6, 3, em.method = "none")
+expect_equal(dfm_large_2s$A[1, 1], 0.6778009, tolerance = 1e-5)
+expect_equal(dfm_large_2s$A[6, 18], 0.1314988, tolerance = 1e-5)
 
 # Now testing DGR 2012 method
 Xcols = colSums(is.na(BM14_M[70:350, ])) == 0
@@ -49,3 +81,4 @@ mod_BM = DFM(diff(X_cc), 4, 3, em.method = "BM")
 expect_equal(mod$F_twostep, mod_BM$F_twostep, tolerance = 1e-2)
 expect_equal(mod$F_qml, mod_BM$F_qml, tolerance = 1e-2)
 expect_equal(mod$A, mod_BM$A, tolerance = 1e-1)
+
