60dbbf70d67beeda686258800ed92080c9675f16
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2022-09-28 00:16:28 +0200

    Commwenting out unused helpers.

--- src/helper.cpp
+++ src/helper.cpp
@@ -18,54 +18,55 @@ arma::cube array2cube( Rcpp::NumericVector myArray ) {
   return(cubedArray);
 }
 
-arma::field<arma::mat> array2field2mat( Rcpp::NumericVector myArray) {
-
-  Rcpp::IntegerVector arrayDims = myArray.attr("dim");
-  Rcpp::NumericVector::iterator it = myArray.begin();
-
-  arma::field<arma::mat> fieldedArray(arrayDims[2], arrayDims[3]);
-
-  for (int i=0; i < arrayDims[2]; i++) {
-    for (int j=0; j < arrayDims[3]; j++) {
-      fieldedArray(i, j) = arma::mat(it, arrayDims[0], arrayDims[1]);
-      it += (arrayDims[0] * arrayDims[1]);
-      }
-  }
-
-  return fieldedArray;
-}
-
-arma::field<arma::cube> array2field1cube( Rcpp::NumericVector myArray) {
-
-  Rcpp::IntegerVector arrayDims = myArray.attr("dim");
-  Rcpp::NumericVector::iterator it = myArray.begin();
-
-  arma::field<arma::cube> fieldedArray(arrayDims[3]);
-
-  for (int i=0; i < arrayDims[3]; i++) {
-    fieldedArray(i) = arma::cube(it, arrayDims[0], arrayDims[1], arrayDims[2]);
-    it += (arrayDims[0] * arrayDims[1] * arrayDims[2]);
-  }
-
-  return fieldedArray;
-}
-
-arma::field<arma::cube> array2field2cube( Rcpp::NumericVector myArray) {
-
-  Rcpp::IntegerVector arrayDims = myArray.attr("dim");
-  Rcpp::NumericVector::iterator it = myArray.begin();
-
-  arma::field<arma::cube> fieldedArray(arrayDims[3], arrayDims[4]);
-
-  for (int i=0; i < arrayDims[3]; i++) {
-    for (int j=0; j < arrayDims[4]; j++) {
-      fieldedArray(i, j) = arma::cube(it, arrayDims[0], arrayDims[1], arrayDims[2]);
-      it += (arrayDims[0] * arrayDims[1] * arrayDims[2]);
-    }
-  }
-
-  return fieldedArray;
-}
+//// Not currently needed...
+// arma::field<arma::mat> array2field2mat( Rcpp::NumericVector myArray) {
+//
+//   Rcpp::IntegerVector arrayDims = myArray.attr("dim");
+//   Rcpp::NumericVector::iterator it = myArray.begin();
+//
+//   arma::field<arma::mat> fieldedArray(arrayDims[2], arrayDims[3]);
+//
+//   for (int i=0; i < arrayDims[2]; i++) {
+//     for (int j=0; j < arrayDims[3]; j++) {
+//       fieldedArray(i, j) = arma::mat(it, arrayDims[0], arrayDims[1]);
+//       it += (arrayDims[0] * arrayDims[1]);
+//       }
+//   }
+//
+//   return fieldedArray;
+// }
+//
+// arma::field<arma::cube> array2field1cube( Rcpp::NumericVector myArray) {
+//
+//   Rcpp::IntegerVector arrayDims = myArray.attr("dim");
+//   Rcpp::NumericVector::iterator it = myArray.begin();
+//
+//   arma::field<arma::cube> fieldedArray(arrayDims[3]);
+//
+//   for (int i=0; i < arrayDims[3]; i++) {
+//     fieldedArray(i) = arma::cube(it, arrayDims[0], arrayDims[1], arrayDims[2]);
+//     it += (arrayDims[0] * arrayDims[1] * arrayDims[2]);
+//   }
+//
+//   return fieldedArray;
+// }
+//
+// arma::field<arma::cube> array2field2cube( Rcpp::NumericVector myArray) {
+//
+//   Rcpp::IntegerVector arrayDims = myArray.attr("dim");
+//   Rcpp::NumericVector::iterator it = myArray.begin();
+//
+//   arma::field<arma::cube> fieldedArray(arrayDims[3], arrayDims[4]);
+//
+//   for (int i=0; i < arrayDims[3]; i++) {
+//     for (int j=0; j < arrayDims[4]; j++) {
+//       fieldedArray(i, j) = arma::cube(it, arrayDims[0], arrayDims[1], arrayDims[2]);
+//       it += (arrayDims[0] * arrayDims[1] * arrayDims[2]);
+//     }
+//   }
+//
+//   return fieldedArray;
+// }
 
 
 // [[Rcpp::export]]
